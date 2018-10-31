a01f4bf28d93ce69ab1bbbc0adf46b0cc50acccd
123 <HelloCXD>
2018-10-31 08:23:52 +0800

    实现了简单的注册登录功能

--- App/urls.py
+++ App/urls.py
@@ -5,10 +5,11 @@ from App import views
 urlpatterns = [
 
     url(r'^$', views.index, name='index'),
-    url(r'^index2$', views.index2, name='index2'),
-    url(r'^register$', views.register, name='register'),
-    url(r'^login$', views.login, name='login'),
-    url(r'^goods$', views.goods, name='goods'),
-    url(r'^cart$', views.cart, name='cart')
+    url(r'^index2/$', views.index2, name='index2'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^goods/$', views.goods, name='goods'),
+    url(r'^cart/$', views.cart, name='cart'),
+    url(r'^quit/$',views.quit, name='quit')
 
 ]
--- App/views.py
+++ App/views.py
@@ -1,22 +1,77 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from App.models import User
 
 
 def index(request):
-    return render(request, 'index.html')
+    tel = request.COOKIES.get('tel')
+    users = User.objects.filter(tel=tel)
+    if users.exists():
+        user = users.first()
+        return render(request, 'index.html', context={'tel': user.tel})
+
+    else:
+        return render(request, 'index.html')
+
+
+
+
 
 
 def index2(request):
-    return render(request, 'index2.html')
+    tel = request.COOKIES.get('tel')
+    users = User.objects.filter(tel=tel)
+    if users.exists():
+        user = users.first()
+        return render(request, 'index2.html', context={'tel': user.tel})
+
+    else:
+        return render(request, 'index2.html')
+
 
 
 def register(request):
-    return render(request, 'register.html')
+    # print(1)
+    if request.method == "GET":
+        return render(request, 'register.html')
+    elif request.method == "POST":
+        # print(2)
+        user = User()
+        user.tel = request.POST.get('tel')
+        user.password = request.POST.get('password')
+        # print(user.tel, user.password)
+        user.save()
+
+        response = redirect('App:index')
+        response.set_cookie('tel', user.tel)
+
+
+        return response
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'GET':
+
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        tel = request.POST.get('tel')
+        password = request.POST.get('password')
+        users= User.objects.filter(tel=tel).filter(password=password)
+        if users.exists():
+            user= users.first()
+            response=redirect('App:index')
+            response.set_cookie('tel', user.tel)
+            return response
+        else:
+            return HttpResponse('用户名或密码错误')
+
+def quit(request):
+    response = redirect('App:index')
+    response.delete_cookie('tel')
+
+    return response
 
 
 def goods(request):
@@ -25,3 +80,5 @@ def goods(request):
 
 def cart(request):
     return render(request, 'cart.html')
+
+
